3617179a40d0727df2c0e11e9ea7ec2cc3afb1de
윤혜원 <yjhyhw8998@gmail.com>
2021-05-29 23:45:51 +0900

    Update clustering.py

--- clustering/clustering.py
+++ clustering/clustering.py
@@ -17,17 +17,16 @@ def dbscan(dataset, eps, minpts): # data : id, x_coord, y_coord, processed
     
     return n - 1
 
-def retrieveDensityReachable(dataset, p, eps, minpts, cluster_n):
+def retrieveDensityReachable(dataset, p, eps, minpts, cluster_id):
     neighbors = getNeighbors(dataset, p, eps)
 
-    if len(neighbors) < minpts: # border point
-        return cluster_n
+    if len(neighbors) < minpts: # outlier
+        return cluster_id
 
     # core point
-    p[3] = cluster_n
+    p[3] = cluster_id
     while True:
-        # all of the points have been processed
-        if len(neighbors) == 0:
+        if len(neighbors) == 0: # all of the points have been processed
             break
         
         neighbor_neighbors = []
@@ -36,15 +35,15 @@ def retrieveDensityReachable(dataset, p, eps, minpts, cluster_n):
             if neighbor[3] > 0: # processed
                 continue
 
-            neighbor[3] = cluster_n
+            neighbor[3] = cluster_id
             new_neighbors = getNeighbors(dataset, neighbor, eps)
 
-            if len(new_neighbors) >= minpts:
+            if len(new_neighbors) >= minpts: # it's not a border point
                 neighbor_neighbors += new_neighbors
 
         neighbors = neighbor_neighbors
     
-    return cluster_n + 1
+    return cluster_id + 1
 
 def getNeighbors(dataset, p, eps):
     neighbors = []
@@ -69,7 +68,12 @@ def selectClusters(dataset, n):
 
     del(clusters[0]) # remove outliers
 
-    if len(clusters) <= n:
+    if len(clusters) < n:
+        for i in range(n - len(clusters)):
+            clusters[-i] = []
+        return clusters
+
+    if len(clusters) == n:
         return clusters
     
     # remove small (n - len(clusters)) clusters
@@ -125,7 +129,7 @@ if __name__ == "__main__":
         print("Insufficient arguments!")
         print("\"clustering.py [input data file name] [# of clusters] [Eps] [MinPts]\"")
 
-    # startTime = time.time()
+    startTime = time.time()
 
     dataset = readInputFile(sys.argv[1])
     numCluster = int(sys.argv[2])
@@ -135,12 +139,12 @@ if __name__ == "__main__":
     n = dbscan(dataset, eps, minpts)
     clusters = selectClusters(dataset, numCluster)
 
-    # print("# of clusters : ", n)
-    # print("select ", numCluster)
-    # for c in clusters:
-    #     print(c, " : ", len(clusters[c]))
+    print("# of clusters : ", n)
+    print("select ", numCluster)
+    for c in clusters:
+        print(c, " : ", len(clusters[c]))
 
     writeOutputFile(sys.argv[1], clusters)
 
-    # finishTime = time.time()
-    # print(finishTime - startTime, " 초 경과")
+    finishTime = time.time()
+    print(finishTime - startTime, " 초 경과")
