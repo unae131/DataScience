765c213dbc969cd547228e877f860277f44506d0
윤혜원 <yjhyhw8998@gmail.com>
2021-06-08 20:23:41 +0900

    Submission0_2

--- recommendation system/recommender.py
+++ recommendation system/recommender.py
@@ -8,41 +8,19 @@ import time
 import numpy as np
 from wrmf import *
 
-def fillRatingMatrix(ratingMatrix, binaryMatrix, theta = 0.5):
+def fillRatingMatrix(ratingMatrix):
     nUsers = len(ratingMatrix)
     nItems = len(ratingMatrix[0])
     ratingMatrix = np.array(ratingMatrix)
 
-    interest = []
-    for i in range(nUsers):
-        for j in range(nItems):
-            if ratingMatrix[i][j] != 0:
-                continue
-            
-            interest.append(binaryMatrix[i][j])
-
-    interest.sort()
-
-    maxUninterest = interest[int(theta * len(interest))]
-    
     for i in range(nUsers):
         for j in range(nItems):
             if ratingMatrix[i][j] != 0:
                 continue
         
-            if binaryMatrix[i][j] > maxUninterest:
-                ratingMatrix[i][j] = 3
-
-            else:
-                ratingMatrix[i][j] = 2
+            ratingMatrix[i][j] = 3
             
     return ratingMatrix
-
-def inferPreUsePrefer(binaryMatrix): # svd
-    u, s, vh = np.linalg.svd(binaryMatrix, full_matrices=False)
-    us = np.matmul(u, np.diag(s))
-    inferedBinaryMatrix = np.matmul(us, vh)
-    return inferedBinaryMatrix
 
 def _getRatingMatrix(dataset):
     users = list(dataset.keys())
@@ -66,18 +44,16 @@ def _getRatingMatrix(dataset):
         i+=1
 
     ratingMatrix = np.full((len(users), len(items)), 0, dtype = np.uint8)
-    binaryMatrix = np.full((len(users), len(items)), 0., dtype = np.float64)
     i = 0
     for user in users:
         j = 0
         for item in items:
             if item in dataset[user]:
                 ratingMatrix[i][j] = dataset[user][item]
-                binaryMatrix[i][j] = 1.
             j += 1
         i += 1
 
-    return userIDs, itemIDs, ratingMatrix, binaryMatrix
+    return userIDs, itemIDs, ratingMatrix
 
 def readInputFile(trainFile):
     with open(trainFile, 'r') as f:
@@ -95,7 +71,7 @@ def readInputFile(trainFile):
 
     return _getRatingMatrix(dataset)
 
-def writeOutputFile(testFileName, ratingMatrix, userIDs, itemIDs):
+def writeOutputFile(testFileName, ratingMatrix, userIDs, itemIDs, default):
     with open(testFileName, 'r') as f:
         lines = f.readlines()
 
@@ -107,7 +83,7 @@ def writeOutputFile(testFileName, ratingMatrix, userIDs, itemIDs):
         try:
             rating = ratingMatrix[userIDs[user]][itemIDs[item]]
         except KeyError:
-            rating = 3
+            rating = round(default)
 
         result += str(user) + "\t" + str(item) + "\t" + str(rating) + "\n"
 
@@ -115,26 +91,6 @@ def writeOutputFile(testFileName, ratingMatrix, userIDs, itemIDs):
     with open(resultFileName, 'w') as f:
         f.write(result)
     
-    return resultFileName
-
-def testRMSE(testFile):
-    with open(testFile, 'r') as f:
-        lines1 = f.readlines()
-
-    resultFile = testFile.split(".")[0] + ".base_prediction.txt"
-    with open(resultFile, 'r') as f:
-        lines2 = f.readlines()
-
-    diffSum = 0
-    for i in range(len(lines1)): # user id | item id | rating | timestamp
-        __, __, answer, __ = list(map(int, lines1[i].split()))
-        __, __, predict = list(map(int, lines2[i].split()))
-
-        diffSum += (answer - predict) ** 2
-
-    rmse = np.sqrt(diffSum / len(lines1))
-    return rmse
-
 if __name__ == "__main__":
     if len(sys.argv) < 3:
         print("Insufficient arguments!")
@@ -143,12 +99,11 @@ if __name__ == "__main__":
     else:
         startTime = time.time()
 
-        userIDs, itemIDs, ratingMatrix, binaryMatrix = readInputFile(sys.argv[1])
-        binaryMatrix = inferPreUsePrefer(ratingMatrix)
-        ratingMatrix = fillRatingMatrix(ratingMatrix, binaryMatrix)
+        userIDs, itemIDs, ratingMatrix = readInputFile(sys.argv[1])
+        ratingMatrix = fillRatingMatrix(ratingMatrix)
 
-        writeOutputFile(sys.argv[2], ratingMatrix, userIDs, itemIDs)
+        mean = np.sum(ratingMatrix) / (len(userIDs) * len(itemIDs))
+        writeOutputFile(sys.argv[2], ratingMatrix, userIDs, itemIDs, mean)
         
         finishTime = time.time()
         print(sys.argv[1], finishTime - startTime, "초")
-        print("rmse", testRMSE(sys.argv[2]))
